6a426bbf8da6bf259c74a3ce1a6a83cfed644ea1
KanchonBauri <kanchonbauri1998@gmail.com>
2024-10-30 21:53:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import StatefullForm from './Component/StatefullForm/StatefullForm'
+// import SimpleForm from './Component/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+    <h1>Vite + React</h1>
+    {/* <SimpleForm></SimpleForm> */}
+    <StatefullForm></StatefullForm>
+    </>
+  )
+}
+
+export default App
